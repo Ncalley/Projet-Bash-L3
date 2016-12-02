21172df6e4eee084967df525c7b874a54c8bcc9b
Ncalley <nicolas.calley@laposte.net>
2016-12-02 10:51:37 +0100

    Add files via upload

--- Projet-Bash-L3/Projet_backup.sh
+++ Projet-Bash-L3/Projet_backup.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
+adresse=""
+nom=""
+source backup.conf
+#if [ -z "$1" ]; then
+#	adresse=$1
+#fi
+#if [ -z "$2" ]; then
+#        nom=$2
+#fi
 
 echo "Sauvegarde du système"
 echo "------------------------------------------------------"
 echo "Entrez l'adresse de votre backup :"
-read adresse
+echo "$adresse ET $nom"
 echo "Création de l'archive"
-tar -cvzf $adresse/backup.tar.gz "/etc/" "/root" "/home" "/var" "/usr/local"
-chmod 700 $adresse/backup.tar.gz
+test ! -d $adresse && mkdir $adresse
+dn=$(dirname $0)
+#tar -czf $dn/$adresse/$nom.tar.gz "/etc" "/root" "/home" "/var" "/usr/local"
+chmod 400 $dn/$adresse/$nom.tar.gz
 echo "------------------------------------------------------"
 echo ""
 echo "Vérification de l'archive"
 
-if [ -e /votre_support/backup.tar.gz ]
+if [ -e $dn/$adresse/$nom.tar.gz ]
 then echo "Votre archive a bien été créée."
 else echo "Il y a eu un problème lors de la création de l'archive."
 fi
 
+echo "Cryptage :"
+gpg --sign $dn/$adresse/$nom.tar.gz || mdp="1" && gpg --gen-key && gpg --sign $dn/$adresse/$nom.tar.gz  
+gpg --verify $dn/$adresse/$nom.tar.gz 
 echo "FIN"
 
