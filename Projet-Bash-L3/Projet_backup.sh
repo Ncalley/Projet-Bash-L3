d9d6bf40348b0ca66b403dceeba234ca06ab0919
Ncalley <nicolas.calley@laposte.net>
2016-12-07 10:49:16 +0100

    Add files via upload

--- Projet-Bash-L3/Projet_backup.sh
+++ Projet-Bash-L3/Projet_backup.sh
@@ -2,7 +2,8 @@
 
 #On initialise les variables
 adresse=""
-nom=""
+name=""
+file=""
 
 #On recupere les données de backup.conf => l'adresse et le nom du fichier par défaut
 source backup.conf
@@ -24,56 +25,69 @@ source backup.conf
 ###############################################
 
 #On lance le menu de sauvegarde
-choix="default"
-while [ "$choix" != "1:" ];do
+choice="default"
+while [ "$choice" != "1:" ];do
 
 	#Le menu
-	if [ "$choix" = "default" ];then
-		choix=$(dialog --stdout --title "Sauvegarde du système" --menu Choix 0 0 0 "1:" "Lancer la backup" "2:" "Choix de l'adresse" "3:" "Choix du nom du fichier backup" "4:" "Choix des fichiers à sauvegarder")
+	if [ "$choice" = "default" ];then
+		choice=$(dialog --stdout --title "Sauvegarde du système" --menu Choix 0 0 0 "1:" "Lancer la backup" "2:" "Modifier l'adresse" "3:" "Modifier le nom du fichier backup" "4:" "Modifier les fichiers à sauvegarder" "5:" "Quitter")
 	fi
 
 	#Changement d'adresse
-	if [ "$choix" = "2:" ];then
+	if [ "$choice" = "2:" ];then
 		adresseInput=$(dialog --stdout --title "Sauvegarde du système" --inputbox "\nEntrez l'adresse de votre backup :" 0 0)
 		if [ "$adresseInput" != "" ]
 		then
 			adresse=$adresseInput
 		fi
-		choix="default"
+		choice="default"
 	fi
 
 	#Changement du nom du fichier de backup
-	if [ "$choix" = "3:" ];then
-		nomInput=$(dialog --stdout --title "Sauvegarde du système" --inputbox "\nEntrez le nom du fichier de backup :" 0 0)
-                if [ "$nomInput" != "" ]
+	if [ "$choice" = "3:" ];then
+		nameInput=$(dialog --stdout --title "Sauvegarde du système" --inputbox "\nEntrez le nom du fichier de backup :" 0 0)
+                if [ "$nameInput" != "" ]
                 then
-                        nom=$nomInput
+                        name=$nameInput
                 fi
-                choix="default"
+                choice="default"
 	fi
 
 	#Changement des fichiers à sauvegarder
-	#choix="default"
+	 if [ "$choice" = "4:" ];then
+                fileInput=$(dialog --stdout --title "Sauvegarde du système" --inputbox "Entrez le nom des fichiers à sauvegarder\nFichier1 Fichier2 etc...\nExemple: /home")
+                if [ "$fileInput" != "" ]
+                then
+                        file=$fileInput
+                fi
+                choice="default"
+        fi
+
+	#Quitter
+	if [ "$choice" = "5:" ];then
+                exit
+        fi
 done
 
 
-dialog --title "Sauvegarde du système" --msgbox "$adresse ET $nom\nCréation de l'archive" 0 0
+dialog --title "Sauvegarde du système" --msgbox "Sauvegarde sur\n$dn/$adresse/$name.tar.gz\nCréation de l'archive" 0 0
 
 test ! -d $adresse && mkdir $adresse dn=$(dirname $0)
 #on effectue la sauvegarde des fichiers donnés
-#tar -czf $dn/$adresse/$nom.tar.gz "/etc" "/root" "/home" "/var" "/usr/local"
-chmod 400 $dn/$adresse/$nom.tar.gz
-echo "------------------------------------------------------"
-echo ""
-echo "Vérification de l'archive"
+#tar -czf $dn/$adresse/$name.tar.gz $file
+chmod 400 $dn/$adresse/$name.tar.gz
 
-if [ -e $dn/$adresse/$nom.tar.gz ]
-then echo "Votre archive a bien été créée."
-else echo "Il y a eu un problème lors de la création de l'archive."
+dialog --title "Sauvegarde du système" --msgbox "Vérification de l'archive" 0 0
+
+if [ -e $dn/$adresse/$name.tar.gz ]
+then
+dialog --title "Sauvegarde du système" --msgbox "Votre archive a bien été créée." 0 0
+else
+dialog --title "Sauvegarde du système" --msgbox "Il y a eu un problème lors de la création de l'archive." 0 0
 fi
 
-echo "Cryptage :"
-#gpg --sign $dn/$adresse/$nom.tar.gz || mdp="1" && gpg --gen-key && gpg --sign $dn/$adresse/$nom.tar.gz  
-#gpg --verify $dn/$adresse/$nom.tar.gz 
-echo "FIN"
+dialog --title "Sauvegarde du système" --msgbox "Cryptage :" 0 0
+#gpg --sign $dn/$adresse/$name.tar.gz || mdp="1" && gpg --gen-key && gpg --sign $dn/$adresse/$name.tar.gz
+#gpg --verify $dn/$adresse/$name.tar.gz
+dialog --title "Sauvegarde du système" --msgbox "FIN" 0 0
 
